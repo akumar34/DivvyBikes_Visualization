36e9a47709d49dd1c06df843aa06ab0810d4e24e
pvempa2@uic.edu <pvempa2@uic.edu>
2014-10-26 20:21:46 -0500

    Adding labels on line chart after clicking ticker

--- App/js/calendar/CalendarControlApp.js
+++ App/js/calendar/CalendarControlApp.js
@@ -37,94 +37,20 @@ var CalendarControlApp = Class.extend({
         var width = this.barCanvasWidth - left - right;
         var height = this.barCanvasHeight - top - bottom;
         var date = this.dateAsString;
-        //var station = this.station;
-    
+        var dataCount = new Array(24);
+    var ticker = this.dateAsString.split(" ")[1];
 
         var graph = this.svgBar1;
 
         graph.selectAll("*").remove();
-        var dataCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    
+    for(var i = 0; i < 24; i++){
+        dataCount[i] = 0;
+    }
         data.forEach(function (d) {
-            d.starttime = new Date(d.starttime);
-            switch (d.starttime.getHours()) {
-                case 0 :
-                    dataCount[0] += 1;
-                    break;
-                case 1 :
-                    dataCount[1] += 1;
-                    break;
-                case 2 :
-                    dataCount[2] += 1;
-                    break;
-                case 3 :
-                    dataCount[3] += 1;
-                    break;
-                case 4 :
-                    dataCount[4] += 1;
-                    break;
-                case 5 :
-                    dataCount[5] += 1;
-                    break;
-                case 6 :
-                    dataCount[6] += 1;
-                    break;
-                case 7 :
-                    dataCount[7] += 1;
-                    break;
-                case 8 :
-                    dataCount[8] += 1;
-                    break;
-                case 9 :
-                    dataCount[9] += 1;
-                    break;
-                case 10 :
-                    dataCount[10] += 1;
-                    break;
-                case 11 :
-                    dataCount[11] += 1;
-                    break;
-                case 12 :
-                    dataCount[12] += 1;
-                    break;
-                case 13 :
-                    dataCount[13] += 1;
-                    break;
-                case 14 :
-                    dataCount[14] += 1;
-                    break;
-                case 15 :
-                    dataCount[15] += 1;
-                    break;
-                case 16 :
-                    dataCount[16] += 1;
-                    break;
-                case 17 :
-                    dataCount[17] += 1;
-                    break;
-                case 18 :
-                    dataCount[18] += 1;
-                    break;
-                case 19 :
-                    dataCount[19] += 1;
-                    break;
-                case 20 :
-                    dataCount[20] += 1;
-                    break;
-                case 21 :
-                    dataCount[21] += 1;
-                    break;
-                case 22 :
-                    dataCount[22] += 1;
-                    break;
-                case 23 :
-                    dataCount[23] += 1;
-                    break;
-                default :
-                    console.log("default case reached... something wrong");
-                    break;
-            }
-            d.stoptime = new Date(d.stoptime);
-        });
+        d.starttime = new Date(d.starttime);
+        dataCount[ d.starttime.getHours() ] += 1;
+    });
 
         var x = d3.scale.linear().domain([0, dataCount.length]).range([0, width]);
         var y = d3.scale.linear().domain([0, d3.max(dataCount)])
@@ -171,6 +97,23 @@ var CalendarControlApp = Class.extend({
             .style("text-anchor", "middle")
             .text("Time Interval")
         ;
+
+    graph.selectAll("label")
+         .data(dataCount.filter(function(d,i){
+               return i === (+ticker);
+          }))
+         .enter()
+         .append("text")
+         .text(function(d){
+              return d;
+         })
+         .attr("x", function(d){
+        return x(ticker);
+        })
+         .attr("y", function(d){
+        return y(d);
+        })
+         .style("font-size", "120%");
 
         graph.append("svg:g")
             .attr("class", "y axis")
@@ -413,14 +356,15 @@ var CalendarControlApp = Class.extend({
     },
 
     monthFormated: function(date){
-    var month = date.getMonth();
+    month = date.getMonth();
         return month < 10 ? "0" + (month+1) : month+1;
     },
 
     dayFormated: function(date){
-       day = date.getDate();
-       return day < 10 ? "0" + (day) : day;
-    }
+    day = date.getDate();
+        return day < 10 ? "0" + (day) : day;
+    },
+
    /* convertToDate: function (dateTime,number){
     var year = dateTime.getFullYear();
     var month = dateTime.getMonth();
