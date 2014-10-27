c49fdb906a30b588663a4e51f6d11fc939517133
Prateek <garg.prateek1@gmail.com>
2014-10-27 06:23:06 -0500

    Abhinav's Changes
    Changes to Map, and Playback with filters

--- App/js/calendar/CalendarControlApp.js
+++ App/js/calendar/CalendarControlApp.js
@@ -13,8 +13,14 @@ var CalendarControlApp = Class.extend({
 
         this.myTag = "";
 
-    this.stationArray = [];
+	this.stationArray = [];
         this.dateAsString = '';
+
+	this.filter = {};
+	this.filter.gender = "";
+	this.filter.members = "";
+	this.filter.age = "";
+	
     },
 
 
@@ -28,8 +34,8 @@ var CalendarControlApp = Class.extend({
     /////////////////////////////////////////////////////////////
 
     initAppWithTickerAndStations: function (stations,ticker){
-    this.stationArray = stations;
-    this.dateAsString = ticker;
+	this.stationArray = stations;
+	this.dateAsString = ticker;
     },
 
     /////////////////////////////////////////////////////////////
@@ -44,19 +50,19 @@ var CalendarControlApp = Class.extend({
         var height = this.barCanvasHeight - top - bottom;
         var date = this.dateAsString;
         var dataCount = new Array(24);
-    var ticker = this.dateAsString.split(" ")[1];
+	var ticker = this.dateAsString.split(" ")[1];
 
         var graph = this.svgBar1;
 
         graph.selectAll("*").remove();
-    
-    for(var i = 0; i < 24; i++){
-        dataCount[i] = 0;
-    }
-        data.forEach(function (d) {
-        d.starttime = new Date(d.starttime);
-        dataCount[ d.starttime.getHours() ] += 1;
-    });
+	
+	for(var i = 0; i < 24; i++){
+		dataCount[i] = 0;
+	}
+     	data.forEach(function (d) {
+		d.starttime = new Date(d.starttime);
+		dataCount[ d.starttime.getHours() ] += 1;
+	});
 
         var x = d3.scale.linear().domain([0, dataCount.length]).range([0, width]);
         var y = d3.scale.linear().domain([0, 80+d3.max(dataCount)])
@@ -100,22 +106,22 @@ var CalendarControlApp = Class.extend({
             .style("text-anchor", "middle")
             .text("Time Interval");
 
-    graph.selectAll("label")
-         .data(dataCount.filter(function(d,i){
-               return i === (+ticker);
-          }))
-         .enter()
-         .append("text")
-         .text(function(d){
-              return d;
-         })
-         .attr("x", function(d){
-        return x(ticker);
-        })
-         .attr("y", function(d){
-        return y(d) ;
-        })
-         .style("font-size", "120%")
+	graph.selectAll("label")
+	     .data(dataCount.filter(function(d,i){
+	           return i === (+ticker);
+	      }))
+	     .enter()
+   	     .append("text")
+	     .text(function(d){
+	          return d;
+	     })
+	     .attr("x", function(d){
+		return x(ticker);
+		})
+	     .attr("y", function(d){
+		return y(d) ;
+		})
+	     .style("font-size", "120%")
              .style("font-color", "steelblue");
 
         graph.append("svg:g")
@@ -130,16 +136,16 @@ var CalendarControlApp = Class.extend({
 
         graph.append("svg:path").attr("d", line(dataCount));
 
-    graph.selectAll(".chart-title")
-        .data(data)
-       .enter()
-       .append("text")
-       .attr("x", width/2)
-       .attr("y", height-500)
-       .attr("text-anchor","middle")
-       .attr("font-family", "sans-serif")
-       .attr("font-size","20pt")
-       .text("Overall Active Rides Line Chart");
+	graph.selectAll(".chart-title")
+		.data(data)
+	   .enter()
+	   .append("text")
+	   .attr("x", width/2)
+	   .attr("y", height-500)
+	   .attr("text-anchor","middle")
+	   .attr("font-family", "sans-serif")
+	   .attr("font-size","20pt")
+	   .text("Overall Active Rides Line Chart");
 
     },
 
@@ -154,53 +160,82 @@ var CalendarControlApp = Class.extend({
         var width = this.barCanvasWidth - left - right;
         var height = this.barCanvasHeight - top - bottom;
         var date = this.dateAsString;
-    var dataCount = new Array(25);    
-    var ticker = this.dateAsString.split(" ")[1];
-    var Stations = this.stationArray;
+ 	var dataCount = new Array(25);    
+	var ticker = this.dateAsString.split(" ")[1];
+	var Stations = this.stationArray;
         var graph = this.svgBar2;
-    var sum = 0;
+	var sum = 0;
+
+	var gender = this.filter.gender;
+	var members = this.filter.members;
+	var age = this.filter.age;
+
         graph.selectAll("*").remove();
-    
+	
 
-    for(var i = 0; i < 25; i++){
-        dataCount[i] = {};
-    }
+	for(var i = 0; i < 25; i++){
+		dataCount[i] = {};
+	}
 
-    for(var i = 0; i < 25; i++){
-        for(var j = 0; j < Stations.length; j++){
-            dataCount[i].intervals = i;
-            dataCount[i][Stations[j]] = 0;
-        }
-    }
+	for(var i = 0; i < 25; i++){
+		for(var j = 0; j < Stations.length; j++){
+			dataCount[i].intervals = i;
+			dataCount[i][Stations[j]] = 0;
+		}
+	}
 
         Stations.forEach(function(s,i){
-            data.forEach(function (d) {
-                d.starttime = new Date(d.starttime);
-            if(d.from_station_name === s){
-                dataCount[ d.starttime.getHours() ].intervals = d.starttime.getHours();
-                dataCount[ d.starttime.getHours() ][Stations[i]] += 1;
+        	data
+			.filter(function(d){
+				if(gender !== "") {
+					if(gender === "All") return true;
+					else return d.gender === gender;
+				}
+				else if(age !== ""){
+					if(age === "All") return true;
+					else if(d.birthday === null || d.birthday === "" || d.birthday === "null") return false;
+					 
+					else {
+						var lower = (+age.split("_")[0]);
+						var upper = (+age.split("_")[1]);
+						return (lower <= (2013 - d.birthday) && (2013 - d.birthday) <= upper);
+					}
+				}
+				else if(members !== ""){
+					if(members === "All") return true;
+					else return d.usertype === members;
+				}
+				else{
+					return true;
+				}
+			})
+			.forEach(function (d) {
+           		d.starttime = new Date(d.starttime);
+			if(d.from_station_name === s){
+				dataCount[ d.starttime.getHours() ].intervals = d.starttime.getHours();
+				dataCount[ d.starttime.getHours() ][Stations[i]] += 1;
 
-            }
-         });
-    });
+			}
+		 });
+	});
         
 
-    Stations.forEach(function(s,i){
-            dataCount.forEach(function (d) {
-                d.intervals = +d.intervals;
-            d[Stations[i]] = +d[Stations[i]];
-         });
-    });
+	Stations.forEach(function(s,i){
+        	dataCount.forEach(function (d) {
+           		d.intervals = +d.intervals;
+			d[Stations[i]] = +d[Stations[i]];
+		 });
+	});
 
         var x = d3.scale.linear().domain([0, dataCount.length]).range([0, width]);
 
-    sum = 0;
+	sum = 0;
 
-    Stations.forEach(function(s){
-        dataCount.forEach(function (d) {
-            sum = sum + d[s];
-         });
-    });
+	Stations.forEach(function(s){
+		dataCount.forEach(function (d) {
+			sum = sum + d[s];
+		 });
+	});
 
         var y = d3.scale.linear().domain([0, sum])
         .range([height, 0]);
@@ -217,22 +252,22 @@ var CalendarControlApp = Class.extend({
             .tickFormat(d3.format(".2s"));
         xAxis.tickSize(-height).tickSubdivide(true);
         
-    var line = d3.svg.line()
+	var line = d3.svg.line()
             .x(
-        function (d) {
-            return x(+d.intervals);
-                }
-        )
+		function (d) {
+			return x(+d.intervals);
+            	}
+	    )
             .y(
-        function (d) {
-            sum = 0;
+		function (d) {
+			sum = 0;
 
-            Stations.forEach(function(s){
-                sum = sum + (+d[s]);
-            });
-            return y(sum);
-                }
-        );
+			Stations.forEach(function(s){
+				sum = sum + (+d[s]);
+			});
+			return y(sum);
+            	}
+	    );
 
         graph
             .attr("width", width)
@@ -253,26 +288,26 @@ var CalendarControlApp = Class.extend({
             .style("text-anchor", "middle")
             .text("Time Interval");
 
-    graph.selectAll("text.label")
-         .data(dataCount.filter(function(d){
-               return d.intervals === (+ticker);
-          }))
-         .enter()
-         .append("text")
-         .text(function(d){
+	graph.selectAll("text.label")
+	     .data(dataCount.filter(function(d){
+	           return d.intervals === (+ticker);
+	      }))
+	     .enter()
+   	     .append("text")
+	     .text(function(d){
                   sum = 0;
-              Stations.forEach(function(s){
-              sum += d[s];
+	          Stations.forEach(function(s){
+		      sum += d[s];
                   });
-          return sum;
+		  return sum;
                })
-         .attr("x", function(d){
-        return x(ticker);
-        })
-         .attr("y", function(d){
-        return y(sum) ;
-        })
-         .style("font-size", "120%")
+	     .attr("x", function(d){
+		return x(ticker);
+		})
+	     .attr("y", function(d){
+		return y(sum) ;
+		})
+	     .style("font-size", "120%")
              .style("font-color", "steelblue");
 
         graph.append("svg:g")
@@ -287,167 +322,39 @@ var CalendarControlApp = Class.extend({
 
         graph.append("svg:path").attr("d", line(dataCount));
 
-    graph.selectAll(".chart-title")
-        .data(data)
-       .enter()
-       .append("text")
-       .attr("x", width/2)
-       .attr("y", height-500)
-       .attr("text-anchor","middle")
-       .attr("font-family", "sans-serif")
-       .attr("font-size","20pt")
-       .text("Overall Active Rides Line Chart");
+	graph.selectAll(".chart-title")
+		.data(data.filter(function(d){
+				if(gender !== "") {
+					if(gender === "All") return true;
+					else return d.gender === gender;
+				}
+				else if(age !== ""){
+					if(age === "All") return true;
+					else if(d.birthday === null || d.birthday === "" || d.birthday === "null") return false;
+					 
+					else {
+						var lower = (+age.split("_")[0]);
+						var upper = (+age.split("_")[1]);
+						return (lower <= (2013 - (+d.birthday)) && (2013 - (+d.birthday)) <= upper);
+					}
+				}
+				else if(members !== ""){
+					if(members === "All") return true;
+					else return d.usertype === members;
+				}
+				else{
+					return true;
+				}
+			}))
+	   .enter()
+	   .append("text")
+	   .attr("x", width/2)
+	   .attr("y", height-500)
+	   .attr("text-anchor","middle")
+	   .attr("font-family", "sans-serif")
+	   .attr("font-size","20pt")
+	   .text("Overall Active Rides Line Chart");
     },
-
-
-
-//Drawing the bar chart for Origin distribution for the second visualization group.
-    /*drawBarChart2: function (error, data) {
-        var top = this.barMargin.top,
-            bottom = this.barMargin.bottom,
-            left = this.barMargin.left,
-            right = this.barMargin.right;
-        var width = this.barCanvasWidth;
-        var height = this.barCanvasHeight;
-        var date = this.dateAsString;
-
-        var Stations = this.stationArray;
-    //var dataCount = new Array(25);
-
-        var svg = this.svgBar2;
-
-        svg.selectAll("*").remove();
-
-    for(var i = 0; i < 25; i++){
-        dataCount[i] = {};
-    }
-    for(var i = 0; i < 25; i++){
-        dataCount[i].intervals = i;
-        dataCount[i][Stations[0]] = 0;
-        dataCount[i][Stations[1]] = 0;
-        dataCount[i][Stations[2]] = 0;
-    }
-        Stations.forEach(function(s,i){
-            data.forEach(function (d) {
-                d.starttime = new Date(d.starttime);
-            if(d.from_station_name === s){
-                dataCount[ d.starttime.getHours() ].intervals = d.starttime.getHours();
-                dataCount[ d.starttime.getHours() ][Stations[i]] += 1;
-            }
-
-            });
-        });
-        
-    dataCount.forEach(function(d) {
-        d.intervals = +d.intervals;
-        d[Stations[0]] = +d[Stations[0]];
-        d[Stations[1]] = +d[Stations[1]];
-        d[Stations[2]] = +d[Stations[2]];
-    });
-
-    var x = d3.time.scale()
-        .range([0, width]);
-
-    var y = d3.scale.linear()
-            .range([height, 0]);    
-    
-    var color = d3.scale.category10();
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-
-    var line = d3.svg.line()
-            .interpolate("basis")
-            .x(function(d) { 
-            return x(d.intervals); 
-             })
-            .y(function(d) { 
-                        return y(d.occurrences); 
-                    });
-
-    color.domain(d3.keys(data[0]).filter(function(key) { 
-        return key !== "intervals";
-    }));
-
-    data.forEach(function(d) {
-        d.intervals = parseDate(d.intervals);
-    });
-
-    var stations = color.domain().map(function(name) {
-        return {
-                name: name,
-                values: data.map(function(d) {
-                    return {intervals: d.intervals, occurrences: +d[name]};
-                })
-            };
-    });
-
-    x.domain(d3.extent(data, function(d) { return d.intervals; }));
-
-    y.domain([
-    d3.min(stations, function(c) { 
-        return d3.min(c.values, function(v) { 
-            return v.occurrences; 
-        }
-    ); }),
-    d3.max(stations, function(c) { 
-        return d3.max(c.values, function(v) { 
-            return v.occurrences; 
-        }
-    ); 
-    }) ]);
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Active Bikes");
-
-    var station = svg.selectAll(".station")
-        .data(stations)
-        .enter().append("g")
-        .attr("class", "station");
-
-    station.append("path")
-        .attr("class", "line")
-        .attr("d", function(d) { 
-            return line(d.values); 
-        })
-        .style("stroke", function(d) { 
-            return color(d.name); 
-        });
-
-    station.append("text")
-        .datum(function(d) { 
-            return {
-                name: d.name, value: d.values[d.values.length - 1]
-            }; 
-        })
-        .attr("transform", function(d) { 
-            return "translate(" + x(d.value.intervals) + "," + y(d.value.occurrences) + ")"; 
-        })
-        .attr("x", 3)
-        .attr("dy", ".35em")
-        .text(function(d) { 
-            return d.name; 
-        });
-    },*/
-
 
     /////////////////////////////////////////////////////////////
 
@@ -482,8 +389,8 @@ var CalendarControlApp = Class.extend({
     /////////////////////////////////////////////////////////////
 
     updateData: function () {
-    var unformattedDate = this.dateAsString.split(" ");
-    var date = new Date(unformattedDate[0]);
+	var unformattedDate = this.dateAsString.split(" ");
+	var date = new Date(unformattedDate[0]);
         var fileToLoad = "App/json/Map/trips_by_day/trips_data_by_day_" + this.monthFormated(date) + "_" + this.dayFormated(date) + "_" + date.getFullYear() + ".csv";
         switch (this.myTag) {
             case "#Vis1":
@@ -515,29 +422,53 @@ var CalendarControlApp = Class.extend({
     },
 
     updateStation: function (element) {
-    var index = element.selectedIndex;
+	var index = element.selectedIndex;
         var selectedItems = element.selectedOptions.valueOf(0);
         for(var index = 0; index < selectedItems.length; index++){
-        this.stationArray.push(selectedItems[index].value);
+		this.stationArray.push(selectedItems[index].value);
         }
     },
 
     monthFormated: function(date){
-    month = date.getMonth();
+	month = date.getMonth();
         return month < 10 ? "0" + (month+1) : month+1;
     },
 
     dayFormated: function(date){
-    day = date.getDate();
+	day = date.getDate();
         return day < 10 ? "0" + (day) : day;
     },
 
+    setGenderFilter: function(element){
+	this.filter.gender = element;
+	this.filter.age = "";
+	this.filter.members = "";
+	this.updateData();
+    },
+
+    setMemberFilter: function(element){
+	this.filter.gender = "";
+	this.filter.age = "";
+	this.filter.members = element;
+	this.updateData();
+    },
+
+    setAgeFilter: function(element){
+	this.filter.gender = "";
+	this.filter.age = element;
+	this.filter.members = "";
+	this.updateData();
+    },
    /* convertToDate: function (dateTime,number){
-    var year = dateTime.getFullYear();
-    var month = dateTime.getMonth();
-    var day = dateTime.getDate();
-    var date = new Date(year,month,day,number,0,0);
-    return date;
+	var year = dateTime.getFullYear();
+	var month = dateTime.getMonth();
+	var day = dateTime.getDate();
+	var date = new Date(year,month,day,number,0,0);
+ 	return date;
     },*/
+
+    calculateAge: function(birthday) { 
+	return 2013 - parseInt(birthday);
+    }
 
 });
